85cdfe433c42df7050e3ea0d9a180bc7b020b1b6
sara ibnabbad <saraibnabbad96@gmail.com>
2022-10-02 19:46:56 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let inputa = document.getElementById("taille");
 let inputb = document.getElementById("poids");
-let btn = document.querySelector("button");
+let btn = document.querySelector("#calc");
 let result = document.querySelector("h2");
+let reset = document.getElementById("reset");
 
 btn.addEventListener("click",function(){
     let taille=Number(inputa.value)/100;
@@ -9,4 +10,10 @@ btn.addEventListener("click",function(){
     let res=(poids/(taille*taille)).toFixed(2);
     console.log(res);
     result.textContent=res
+})
+
+reset.addEventListener("click",function(){
+    inputa.value="";
+    inputb.value="";
+    result.textContent="";
 })
